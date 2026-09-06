6af8614370365b541ff120d06713f601abcb3175
Amrutha V <amruthabhatv@gmail.com>
2026-09-06 21:23:29 +0530

    Time: 0 ms (100%), Space: 29.8 MB (27.56%) - LeetHub

--- 3740-minimum-distance-between-three-equal-elements-i/3740-minimum-distance-between-three-equal-elements-i.cpp
+++ 3740-minimum-distance-between-three-equal-elements-i/3740-minimum-distance-between-three-equal-elements-i.cpp
@@ -4,7 +4,7 @@ public:
         unordered_map<int, vector<int>> mp;
 
         for (int i = 0; i < nums.size(); i++) {
-            mp[nums[i]].push_back(i+1);
+            mp[nums[i]].push_back(i);
         }
 
         int ans = INT_MAX;

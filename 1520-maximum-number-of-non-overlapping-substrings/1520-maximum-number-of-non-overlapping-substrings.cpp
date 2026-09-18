d32dbc8a4212cc3819204dd4cb5fb7337e0dec66
Amrutha V <amruthabhatv@gmail.com>
2026-09-18 18:40:41 +0530

    Time: 13 ms (55.21%), Space: 23.6 MB (43.46%) - LeetHub

---
+++ 1520-maximum-number-of-non-overlapping-substrings/1520-maximum-number-of-non-overlapping-substrings.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    vector<string> maxNumOfSubstrings(string s) {
+        vector<int> first(26, s.size()), last(26, -1);
+
+        // First and last occurrence
+        for (int i = 0; i < s.size(); i++) {
+            first[s[i] - 'a'] = min(first[s[i] - 'a'], i);
+            last[s[i] - 'a'] = i;
+        }
+
+        vector<pair<int,int>> intervals;
+
+        // Build minimal valid interval for each character
+        for (int c = 0; c < 26; c++) {
+            if (last[c] == -1) continue;
+
+            int l = first[c];
+            int r = last[c];
+            bool valid = true;
+
+            for (int i = l; i <= r; i++) {
+                int ch = s[i] - 'a';
+                if (first[ch] < l) {
+                    valid = false;
+                    break;
+                }
+                r = max(r, last[ch]);
+            }
+
+            if (valid)
+                intervals.push_back({l, r});
+        }
+
+        // Greedy by ending index
+        sort(intervals.begin(), intervals.end(),
+             [](auto &a, auto &b) {
+                 if (a.second == b.second)
+                     return a.first < b.first;
+                 return a.second < b.second;
+             });
+
+        vector<string> ans;
+        int end = -1;
+
+        for (auto &p : intervals) {
+            if (p.first > end) {
+                ans.push_back(s.substr(p.first, p.second - p.first + 1));
+                end = p.second;
+            }
+        }
+
+        return ans;
+    }
+};
